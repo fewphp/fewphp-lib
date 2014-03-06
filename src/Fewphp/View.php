7fcbbfa8a2ff7349bb10b4b527da498548cc8774
qing <qing@Breeze.(none)>
2014-03-06 17:42:02 +0800

    debug

--- src/Fewphp/View.php
+++ src/Fewphp/View.php
@@ -3,6 +3,10 @@
 namespace fewphp;
 
 use DebugBar\StandardDebugBar;
+use Assetic\Asset\AssetCollection;
+use Assetic\Asset\FileAsset;
+use Assetic\Filter\Yui\JsCompressorFilter as JsYuiCompressorFilter;
+use Assetic\Filter\Yui\CssCompressorFilter as CssYuiCompressorFilter;
 
 class View {
 
@@ -19,8 +23,56 @@ class View {
                 $debugbar = new StandardDebugBar();
                 $debugbarRenderer = $debugbar->getJavascriptRenderer();
 
-                $debugbar["messages"]->addMessage("hello world!");
-                echo $debugbarRenderer->renderHead();
+                list($cssFiles, $jsFiles) = $debugbarRenderer->getAssets();
+
+                if (!file_exists(JS . 'debug.js')) {
+                    $js = new AssetCollection(array(
+                        new FileAsset($jsFiles[0]),
+                        new FileAsset($jsFiles[1]),
+                        new FileAsset($jsFiles[2]),
+                        new FileAsset($jsFiles[3]),
+                        ), array(
+                        new JsYuiCompressorFilter(VENDOR . 'nervo/yuicompressor/yuicompressor.jar'),
+                    ));
+
+                    $strJs = $js->dump();
+                    file_put_contents(JS . 'debug.js', $strJs);
+                }
+
+                if (!file_exists(CSS . 'debug.css')) {
+
+                    $css = new AssetCollection(array(
+                        new FileAsset($cssFiles[0]),
+                        new FileAsset($cssFiles[1]),
+                        new FileAsset($cssFiles[2]),
+                        new FileAsset($cssFiles[3]),
+                        ), array(
+                        new CssYuiCompressorFilter(VENDOR . 'nervo/yuicompressor/yuicompressor.jar'),
+                    ));
+
+                    $strCss = $css->dump();
+                    file_put_contents(CSS . 'debug.css', $strCss);
+                }
+                
+                
+                if (!file_exists(IMAGES . 'debug.css')) {
+
+                    $css = new AssetCollection(array(
+                        new FileAsset($cssFiles[0]),
+                        new FileAsset($cssFiles[1]),
+                        new FileAsset($cssFiles[2]),
+                        new FileAsset($cssFiles[3]),
+                        ), array(
+                        new CssYuiCompressorFilter(VENDOR . 'nervo/yuicompressor/yuicompressor.jar'),
+                    ));
+
+                    $strCss = $css->dump();
+                    file_put_contents(CSS . 'debug.css', $strCss);
+                }
+                
+
+                echo '</style><link rel="stylesheet" href="/css/debug.css" />'
+                . '<script type="text/javascript" src="/js/debug.js"></script>';
                 echo $debugbarRenderer->render();
                 break;
             default:
