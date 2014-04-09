77b7027125da6427b688754af552bc06f9cb25d2
Breezess <s88858@163.com>
2014-04-09 23:28:22 +0800

    flash

--- src/Fewphp/Session.php
+++ src/Fewphp/Session.php
@@ -22,4 +22,44 @@ class Session {
         session_destroy();
     }
 
+    public static function check() {
+
+    }
+
+    public function flash($key = 'flash', $attrs = array()) {
+        $out = false;
+
+        if (CakeSession::check('Message.' . $key)) {
+            $flash = self::read('Message.' . $key);
+            $message = $flash['message'];
+            unset($flash['message']);
+
+            if (!empty($attrs)) {
+                $flash = array_merge($flash, $attrs);
+            }
+
+            if ($flash['element'] === 'default') {
+                $class = 'message';
+                if (!empty($flash['params']['class'])) {
+                    $class = $flash['params']['class'];
+                }
+                $out = '<div id="' . $key . 'Message" class="' . $class . '">' . $message . '</div>';
+            }
+            elseif (!$flash['element']) {
+                $out = $message;
+            }
+            else {
+                $options = array();
+                if (isset($flash['params']['plugin'])) {
+                    $options['plugin'] = $flash['params']['plugin'];
+                }
+                $tmpVars = $flash['params'];
+                $tmpVars['message'] = $message;
+                $out = $this->_View->element($flash['element'], $tmpVars, $options);
+            }
+            self::delete('Message.' . $key);
+        }
+        return $out;
+    }
+
 }
--- src/Fewphp/View.php
+++ src/Fewphp/View.php
@@ -5,9 +5,7 @@ namespace fewphp;
 class View {
 
     public $title = '';
-    
     public $url = array();
-    
     public $sqlLog = array();
 
     public function render($name) {
@@ -20,10 +18,12 @@ class View {
                 $sql_dump = '';
                 if (!empty($this->sqlLog)) {
                     foreach ($this->sqlLog as $nr => $sql) {
-                        $sql_dump .= "<tr><td>" .$nr. "</td><td> " . $sql . " </td></tr>";
+                        $sql_dump .= "<tr><td>" . $nr . "</td><td> " . $sql . " </td></tr>";
                     }
                 }
                 echo '<table class="sql_dump"><tr><th>Nr</th><th>Query</th></tr>' . $sql_dump . '</table>';
+                break;
+            case 'flash':
                 break;
             default:
                 $this->url[1] = isset($this->url[1]) ? strtolower($this->url[1]) : 'index';
@@ -39,13 +39,13 @@ class View {
     public function layout($layout) {
         require APP . VIEW . 'layout' . DS . $layout . '.php';
     }
-    
+
     /**
      * 记录sql
      * @param type $sql
      */
     public function log($sql) {
-        $this->sqlLog[] = $sql; 
+        $this->sqlLog[] = $sql;
     }
 
 }
